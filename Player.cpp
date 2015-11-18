82c34b388aafb19551209a942cf2c0f739ccbcfa
Kasiah Butler <snipercorgii@gmail.com>
2015-11-17 16:03:40 -0800

    Filled Player Main
    Player Main Filled In

--- Player.cpp
+++ Player.cpp
@@ -1,1 +1,21 @@
 #include "Player.h"
+
+void Player::Movement()
+{
+
+}
+
+void Fire()
+{
+
+}
+
+void Player::onCollision(GameObject &object, float distance)
+{
+
+}
+
+void Player::update()
+{
+
+}
--- Player.h
+++ Player.h
@@ -3,5 +3,20 @@
 
 class Player : public GameObject
 {
+public:
+	float speed;
+	float rateOfFire;
+	int health;
 
+	Player() : speed(100), rateOfFire(0.5f), health(5)
+	{
+		width = 80; height = 80; x = 400; y = 40;
+		animName = "deerRun";
+		textureName = "deer";
+	}
+
+	void Movement();
+	void Fire();
+	void onCollision(GameObject &object, float distance);
+	void update();
 };
